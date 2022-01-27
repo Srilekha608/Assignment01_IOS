ff537d5a95d8f1945ec440fdb1305542d5b0f4ae
Srilekha608 <64957724+Srilekha608@users.noreply.github.com>
2022-01-27 09:52:02 -0600

    Add files via upload

---
+++ Assignment01.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var distance:Double
+let maxWeight:Int = 130;
+print("Your max weight is \(maxWeight) pounds");
+print ("""
+       Hello,All
+       Welcome to Swift Programming..!
+       """);
+
+var x = 15
+var y = 25.0
+//y = x
+//cannot assign int type to a type double
