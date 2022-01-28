823d38b9e6b0317626add18d4f70eba7a814e2e6
Srilekha608 <64957724+Srilekha608@users.noreply.github.com>
2022-01-27 20:08:18 -0600

    Updated the files

--- Assignment01.playground/Contents.swift
+++ Assignment01.playground/Contents.swift
@@ -8,7 +8,59 @@ print ("""
        Welcome to Swift Programming..!
        """);
 
-var x = 15
-var y = 25.0
+//var x:Double = 15
+//var y = 25.0
 //y = x
 //cannot assign int type to a type double
+//y=x
+//print(y)
+
+//3A
+
+var x = 2;
+var y = 7;
+var z = 5;
+
+if((x > y) && (x > z))
+{
+    print(" \(x) is largest among all the three integers")
+}
+ else if((y > x) && (y > z))
+{
+    print(" \(y) is largest among all the three integers")
+}
+else
+{
+    print(" \(z) is largest among all the three integers")
+    
+}
+
+//3B
+
+var a = 13 ;
+var b =  103;
+
+if (a % 10 == b % 10 )
+{
+ print ("The last two digits of the two given integer values are same")
+}
+else {
+    print("The last two digits of the two given integer values are same")
+}
+
+//4A
+var N = 10;
+var j:Int ;
+var i:Int=0;
+for i in (1...N)
+{
+  print(i)
+   for j in stride(from: 10, to: 1, by: -1)
+    {
+        print(j)
+       }
+    break;
+
+  
+    
+}
